df492279a477f8bcd68e36705810513163f0a41e
Felipe Contratres <felipe.contratres@gmail.com>
2015-07-20 10:54:01 -0300

    file with shared functions

--- utils.py
+++ utils.py
@@ -42,3 +42,42 @@ def tfidf(tokens, idfs):
     tfs = tf(tokens)
     tfIdfDict = {k: v*idfs[k] for k, v in tfs.items()}
     return tfIdfDict
+
+
+def dotprod(a, b):
+    """ Compute dot product
+    Args:
+        a (dictionary): first dictionary of record to value
+        b (dictionary): second dictionary of record to value
+    Returns:
+        dotProd: result of the dot product with the two input dictionaries
+    """
+    dp=0
+    for k in a:
+        if k in b:
+            dp += a[k] * b[k]
+    return  dp
+
+def norm(a):
+    """ Compute square root of the dot product
+    Args:
+        a (dictionary): a dictionary of record to value
+    Returns:
+        norm: a dictionary of tokens to its TF values
+    """
+    return math.sqrt(dotprod(a, a))
+
+def cosineSimilarity(record, idfsRDD, idfsRDD2, corpusNorms1, corpusNorms2):
+    """ Compute Cosine Similarity using Broadcast variables
+    Args:
+        record: ((ID1, ID2), token), RDDs (Broadcast) with idfs and norm values
+    Returns:
+        pair: ((ID1, ID2), cosine similarity value)
+    """
+    vect1Rec = record[0][0]
+    vect2Rec = record[0][1]
+    tokens = record[1]
+    s = sum((idfsRDD[vect1Rec][i]*idfsRDD2[vect2Rec][i] for i in tokens))
+    value = s/((corpusNorms1[vect1Rec])*(corpusNorms2[vect2Rec]))
+    key = (vect1Rec, vect2Rec)
+    return (key, value)
