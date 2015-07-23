b85634f8dfc683dc3ebc6cbaf654eeaac3607b06
Felipe Contratres <felipe.contratres@gmail.com>
2015-07-23 17:03:31 -0300

    adding the tokens on mongo

--- train_classifier.py
+++ train_classifier.py
@@ -1,4 +1,4 @@
-import sys, os, math
+import sys, os, math, datetime
 from timeit import default_timer as timer
 import re, unicodedata
 import nltk
@@ -61,6 +61,34 @@ def findProductsByCategory(categories):
 
 	return product_list
 
+def insertTokensAndCategories(tokens, category):
+    db = createMongoDBConnection(host, port, username, password, database)
+
+    modelCollection = db.model
+    modelCollection.remove({'_type':'token'})
+
+    document_mongo =  dict()
+    document_mongo['_type'] = 'token'
+    document_mongo['_datetime'] = datetime.datetime.utcnow()
+    i = 0
+    for t in tokens:
+        document_mongo[t] = i
+        i = i + 1   
+
+    modelCollection.insert_one(document_mongo)
+
+    modelCollection.remove({'_type':'category'})
+
+    document_mongo =  dict()
+    document_mongo['_type'] = 'category'
+    document_mongo['_datetime'] = datetime.datetime.utcnow()
+    i = 0
+    for c in category:
+        document_mongo[c] = i
+        i = i + 1 
+
+    modelCollection.insert_one(document_mongo)
+
 def tf(tokens):
     """ Compute TF
     Args:
@@ -108,55 +136,55 @@ def tfidf(tokens, idfs):
     return tfIdfDict
 
 def main(sc):
+    start = timer()
+
     categs = ["Computers & Tablets", "Video Games", "TV & Home Theater"]# , "Musical Instruments"]
 
     stpwrds = stopwords.words('english')
-    tbl_translate = dict.fromkeys(i for i in xrange(sys.maxunicode) if unicodedata.category(unichr(i)).startswith('P') or unicodedata.category(unichr(i)).startswith('N'))
+    tbl_translate = dict.fromkeys(i for i in xrange(sys.maxunicode) if unicodedata.category(unichr(i)).startswith('S') or unicodedata.category(unichr(i)).startswith('P') or unicodedata.category(unichr(i)).startswith('N'))
 
     productRDD = sc.parallelize(findProductsByCategory(categs))
     corpusRDD = (productRDD.map(lambda s: (s[0], word_tokenize(s[1].translate(tbl_translate).lower()), s[2], s[3]))
 						   .map(lambda s: (s[0], [PorterStemmer().stem(x) for x in s[1] if x not in stpwrds], s[2], s[3] )))
 
-    
     idfsRDD = idfs(corpusRDD)
     idfsRDDBroadcast = sc.broadcast(idfsRDD.collectAsMap())
     tfidfRDD = corpusRDD.map(lambda x: (x[0], tfidf(x[1], idfsRDDBroadcast.value), x[2], x[3]))
 
-    start = timer()
-    tokens = corpusRDD.flatMap(lambda x: x[1]).distinct().collect()
     category = productRDD.map(lambda x: x[2]).distinct().collect()
+    tokens = corpusRDD.flatMap(lambda x: x[1]).distinct().collect()
 
-    #de alguma forma eu preciso guardar a lista de categorias para poder utilizar quando for predizer depois
+    insertTokensAndCategories(tokens, category)
+
     classifier = Classifier(sc, 'NaiveBayes')
     trainingVectSpaceCategoryRDD, testVectSpaceCategoryRDD = classifier.createVectSpaceCategory(tfidfRDD, category, tokens).randomSplit([8, 2], seed=0L)
     modelNaiveBayesCategory = classifier.trainModel(trainingVectSpaceCategoryRDD, '/dados/models/naivebayes/category')
 
-    #predictionAndLabelCategoryRDD = testVectSpaceCategoryRDD.map(lambda p : (category[int(modelNaiveBayesCategory.predict(p.features))], category[int(p.label)]))
-    #acuraccyCategory = float(predictionAndLabelCategoryRDD.filter(lambda (x, v): x[0] == v[0]).count())/float(predictionAndLabelCategoryRDD.count())
-    #print 'the accuracy of the Category Naive Bayes model is %f' % acuraccyCategory
+    predictionAndLabelCategoryRDD = testVectSpaceCategoryRDD.map(lambda p : (category[int(modelNaiveBayesCategory.predict(p.features))], category[int(p.label)]))
+    acuraccyCategory = float(predictionAndLabelCategoryRDD.filter(lambda (x, v): x[0] == v[0]).count())/float(predictionAndLabelCategoryRDD.count())
+    print 'the accuracy of the Category Naive Bayes model is %f' % acuraccyCategory
 
     #training in this second way just for test
     categoryAndSubcategory = productRDD.map(lambda x: (x[2], x[3])).distinct().collect()
     trainingVectSpaceSubcategory, testVectSpaceSubcategory = classifier.createVectSpaceSubcategory(tfidfRDD, categoryAndSubcategory, tokens).randomSplit([8, 2], seed=0L)
     modelNaiveBayesSubcategory = classifier.trainModel(trainingVectSpaceSubcategory, '/dados/models/naivebayes/subcategory')
 
-    #predictionAndLabelSubategory = testVectSpaceSubcategory.map(lambda p : (categoryAndSubcategory[int(modelNaiveBayesSubcategory.predict(p.features))], categoryAndSubcategory[int(p.label)]))
-    #acuraccySubcategory = float(predictionAndLabelCategory.filter(lambda (x, v): x[0] == v[0]).count())/float(predictionAndLabelCategory.count())
-    #print 'the accuracy of the Subcategory Naive Bayes model is %f' % acuraccySubcategory
+    predictionAndLabelSubcategory = testVectSpaceSubcategory.map(lambda p : (categoryAndSubcategory[int(modelNaiveBayesSubcategory.predict(p.features))], categoryAndSubcategory[int(p.label)]))
+    acuraccySubcategory = float(predictionAndLabelSubcategory.filter(lambda (x, v): x[0] == v[0]).count())/float(predictionAndLabelSubcategory.count())
+    print 'the accuracy of the Subcategory Naive Bayes model is %f' % acuraccySubcategory
 
     #test with DecisionTree Model
     classifierDT = Classifier(sc, 'DecisionTree')
     trainingVectSpaceCategory, testVectSpaceCategory = classifierDT.createVectSpaceCategory(tfidfRDD, category, tokens).randomSplit([8, 2], seed=0L)
     modelDecisionTreeCategory = classifierDT.trainModel(trainingVectSpaceCategory, '/dados/models/dt/category')
 
-    #predictions = modelDecisionTreeCategory.predict(testVectSpaceCategory.map(lambda x: x.features))
-    #predictionAndLabelCategory = testVectSpaceCategory.map(lambda lp: lp.label).zip(predictions)
-    #acuraccyDecisionTree = float(predictionAndLabelCategory.filter(lambda (x, v): x == v).count())/float(predictionAndLabelCategory.count())   
-    #print 'the accuracy of the Decision Tree model is %f' % acuraccyDecisionTree
+    predictions = modelDecisionTreeCategory.predict(testVectSpaceCategory.map(lambda x: x.features))
+    predictionAndLabelCategory = testVectSpaceCategory.map(lambda lp: lp.label).zip(predictions)
+    acuraccyDecisionTree = float(predictionAndLabelCategory.filter(lambda (x, v): x == v).count())/float(predictionAndLabelCategory.count())   
+    print 'the accuracy of the Decision Tree model is %f' % acuraccyDecisionTree
 
     elap = timer()-start
     print 'it tooks %d seconds' % elap
-    print category
 
 if __name__ == '__main__':
     conf = SparkConf().setAppName(APP_NAME)
