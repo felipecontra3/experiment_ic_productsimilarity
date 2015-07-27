61e51f665ad4219af28ad17b749096eb95f3836e
Felipe Contratres <felipe.contratres@gmail.com>
2015-07-27 10:26:21 -0300

    provision

--- provision.sh
+++ provision.sh
@@ -4,18 +4,24 @@ apt-get update
 
  
 echo '----------------------------------------------'
-echo ' INSTALLING JAVA, GIT and MAVEN'
+echo ' INSTALLING JAVA                              '
 echo '----------------------------------------------'
 apt-get -y --force-yes install \
 openjdk-7-jdk \
 htop 
 
+echo '----------------------------------------------'
+echo ' INSTALLING SPARK                             '
+echo '----------------------------------------------'
 mkdir /dados
 chown -R vagrant:vagrant /dados
 wget -P /dados http://ftp.unicamp.br/pub/apache/spark/spark-1.4.1/spark-1.4.1-bin-hadoop2.6.tgz
 tar -zxvf /dados/spark-1.4.1-bin-hadoop2.6.tgz -C /dados/
 rm /dados/spark-1.4.1-bin-hadoop2.6.tgz 
 
+echo '----------------------------------------------'
+echo ' INSTALLING PYTHON STUFF                          '
+echo '----------------------------------------------'
 apt-get -y --force-yes install \
 python-pip \
 python-numpy
